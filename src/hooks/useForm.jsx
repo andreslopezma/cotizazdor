f166cbfcd7464de8f9489e75ef3802b4bf902696
andreslopezma <andres.lopezma@hotmail.com>
2023-09-22 18:24:06 -0500

    Se elimian metodo que ya no se usa

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -7,19 +7,8 @@ const useForm = (initState) => {
         setFormulario({ ...formulario, [name]: value });
     };
 
-    const handleInputMulti = ({ target }) => {
-        const { selectedOptions } = target;
-        let movies = [];
-        for (var i = 0; i < selectedOptions.length; i++) {
-            const { value } = selectedOptions.item(i);
-            movies.push(value);
-        }
-        setFormulario({ ...formulario, 'movies': movies });
-    }
-
     return {
         handleInputChange,
-        handleInputMulti,
         setFormulario,
         formulario
     }
