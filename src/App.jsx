f15500a02a00cf9061affee75550d957534eb94b
andreslopezma <andres.lopezma@hotmail.com>
2023-09-22 17:59:14 -0500

    Se crea el menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,108 @@
+import * as React from 'react';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import { useTheme } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+// styles 
+import { AppBar, Drawer, DrawerHeader } from './assets/NavStyles';
+
+// routes 
+import { Link, Route, Routes } from "react-router-dom";
+
+// components
+import Cotizaciones from './components/Cotizaciones';
+import CrearCotizacion from './components/CrearCotizacion';
+
 function App() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
   return (
     <>
-      <h1>Estoy listo</h1>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar position="fixed" open={open}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              sx={{
+                marginRight: 5,
+                ...(open && { display: 'none' }),
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap component="div">
+              <Link style={{ 'textDecoration': 'none', 'color': 'white' }} to={'/'}> Cotizador</Link>
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={open}>
+          <DrawerHeader>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
+          <List>
+            {[{ name: 'Cotizar', path: '/crear/cotizacion' }].map(({ name, path }, index) => (
+              <Link style={{ 'textDecoration': 'none', 'color': 'black' }} to={`${path.toLocaleLowerCase()}`} key={index}>
+                <ListItem key={name} disablePadding sx={{ display: 'block' }}>
+                  <ListItemButton
+                    sx={{
+                      minHeight: 48,
+                      justifyContent: open ? 'initial' : 'center',
+                      px: 2.5,
+                    }}
+                  >
+                    <ListItemIcon
+                      sx={{
+                        minWidth: 0,
+                        mr: open ? 3 : 'auto',
+                        justifyContent: 'center',
+                      }}
+                    >
+                      {<AttachMoneyIcon />}
+                    </ListItemIcon>
+                    <ListItemText primary={name} sx={{ opacity: open ? 1 : 0 }} />
+                  </ListItemButton>
+                </ListItem>
+              </Link>
+            ))}
+          </List>
+        </Drawer>
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <DrawerHeader />
+          <Routes>
+            <Route path="/" element={<Cotizaciones />} />
+            <Route path="/crear/cotizacion" element={<CrearCotizacion />} />
+          </Routes>
+        </Box>
+      </Box>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
