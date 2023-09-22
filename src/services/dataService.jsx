907f5a0e65e8218033232948c61f907bfc30c5b6
andreslopezma <andres.lopezma@hotmail.com>
2023-09-22 18:02:50 -0500

    Se crea el archvio dataService, el cual se encarga de centralizar todas las peticiones a la api

---
+++ src/services/dataService.jsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+export const fetchData = async ({ url }) => {
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const deleteData = async ({ url }) => {
+    try {
+        const response = await axios.delete(url);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const createData = async ({ url, params }) => {
+    try {
+        const response = await axios.post(url, transformData(params));
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const transformData = (data) => {
+    const formData = new FormData();
+    Object.keys(data).forEach(name => {
+        formData.append([name], data[name]);
+    });
+    return formData;
+}
